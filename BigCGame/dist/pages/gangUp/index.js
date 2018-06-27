bdbe8262b23cb561ba5b0c73bb3f767e084e2d50
lucien0coder <lucien0coder@gmail.com>
2018-06-27 23:48:46 +0800

    添加排名列表与开黑地点搜索

---
+++ BigCGame/dist/pages/gangUp/index.js
@@ -0,0 +1,14 @@
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.default = Page({
+  data: {
+    location: [
+      {value:'1', title: '三水'},
+      {value:'2', title: '禅城'},
+      {value:'3', title: '三山'}
+      ]
+  }
+});
---
+++ BigCGame/dist/pages/integralRank/index.js
@@ -0,0 +1,35 @@
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.default = Page({
+  data: {
+    integralRankList: [{
+      "name": "Jiuzhe Li",
+      "integrals": "160",
+      "src": "https://s10.mogucdn.com/mlcdn/c45406/170603_55gkg003el4k2jbg2d2ieh030fjcf_38x38.png",
+      "record": "胜-25, 平-3, 输-3"
+    }, {
+      "name": "Kao zou",
+      "integrals": "141",
+      "src": "https://s10.mogucdn.com/mlcdn/c45406/170603_55gkg003el4k2jbg2d2ieh030fjcf_38x38.png",
+      "record": "胜-18, 平-3, 输-8"
+    }, {
+      "name": "Meili Zhu",
+      "integrals": "121",
+      "src": "https://s10.mogucdn.com/mlcdn/c45406/170603_55gkg003el4k2jbg2d2ieh030fjcf_38x38.png",
+      "record": "胜-8, 平-13, 输-23"
+    }, {
+      "name": "HuiLing Chen",
+      "integrals": "60",
+      "src": "https://s10.mogucdn.com/mlcdn/c45406/170603_55gkg003el4k2jbg2d2ieh030fjcf_38x38.png",
+      "record": "胜-5, 平-13, 输-14"
+    }, {
+      "name": "DOT Cen",
+      "integrals": "40",
+      "src": "https://s10.mogucdn.com/mlcdn/c45406/170603_55gkg003el4k2jbg2d2ieh030fjcf_38x38.png",
+      "record": "胜-5, 平-13, 输-23"
+    }]
+  }
+});
